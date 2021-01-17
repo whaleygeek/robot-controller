109a8cb056929e07188e1c6ee440159c0ae19042
David Whale <david@thinkingbinaries.com>
2021-01-17 21:00:12 +0000

    Changed demo and robot so that it works like a real robot. Temporarily cooperatively call robot.move() in the demo loop

--- src/demo.py
+++ src/demo.py
@@ -5,18 +5,37 @@ import sys
 if sys.hexversion < 0x03000000:
     exit("Please use python3")
 
-import rcontrol as rc
+import time
+
+import rcontrol as remote
 import robot
 
 robbie = robot.VisualRobot()
 
+def forward(rate=100):
+    # change this to move your robot forward
+    robbie.forward(rate)
+
+def backward(rate=100):
+    # change this to move your robot backward
+    robbie.backward(rate)
+
+def stop():
+    # change this to stop your robot
+    robbie.stop()
+
+def steer(pos=0):
+    # change this to steer your robot
+    robbie.steer(pos)
+
 while True:
-    changed, gear, speed, direction = rc.sense()
+    changed, g, r, s = remote.sense()
     if changed:
-        if   gear == rc.FORWARD:    robbie.forward(speed)
-        elif gear == rc.BACKWARD:   robbie.backward(speed)
-        elif gear == rc.STOP:       robbie.stop()
-        robbie.steer(direction)
-
-
+        if   g == remote.FORWARD:    forward(r)
+        elif g == remote.BACKWARD:   backward(r)
+        elif g == remote.STOP:       stop()
+        steer(s)
+    robbie.move() ## TODO do with a thread in robot.py
+    time.sleep(0.1)
+    
 # END
--- src/robot.py
+++ src/robot.py
@@ -2,8 +2,14 @@
 # Simple simulation of a robot - replace with your own robot routines.
 
 import turtle
+# import thread
 
 class VisualRobot():
+    STOP     = "s"
+    FORWARD  = "f"
+    BACKWARD = "b"
+    BOUNCE   = 10
+
     def __init__(self):
         turtle.forward(0)  # cause turtle canvas to display
         w, h = turtle.screensize()
@@ -12,44 +18,65 @@ class VisualRobot():
         self._YMIN = -(h/2)
         self._YMAX = h/2
 
-    def forward(self, speed:int=10):
-        turtle.forward(speed)
+        self._gear  = self.STOP
+        self._rate  = 0
+        self._steer = 0
 
-    def backward(self, speed:int=10):
-        turtle.backward(speed)
+    def forward(self, rate:int=10):
+        self._gear = self.FORWARD
+        self._rate = rate
+
+    def backward(self, rate:int=10):
+        self._gear = self.BACKWARD
+        self._rate = rate
 
     def stop(self):
-        pass
+        self._gear = self.STOP
+        self._rate = 0
+        self._steer = 0
 
-    def steer(self, direction:int=0):
-        # direction is -100..+100 (an integer percentage), 0 is no steer
-        if direction != 0:
-            direction /= 5
-            if direction < 0:
-                turtle.left(direction)
+    def steer(self, amount:int=0):
+        self._steer = amount
+
+    def move(self):
+        if self._gear == self.STOP: return  # no action unless moving
+
+        # Update rotation on each move, based on steering
+        # steer is -100..+100 (an integer percentage), 0 is no steer
+        if self._steer != 0:
+            r = self._steer / 5
+            if r < 0:
+                turtle.left(r)
             else:
-                turtle.right(direction)
+                turtle.right(r)
 
-            self.check_edges(turtle.xcor(), turtle.ycor())
-            #print(self._XMAX, self._YMAX, turtle.xcor(), turtle.ycor())
+        # move forward or backward based on the gear
+        speed = self._rate
+        if self._gear == self.FORWARD:
+            turtle.forward(speed)
+        elif self._gear == self.BACKWARD:
+            turtle.backward(speed)
 
-    def check_edges(self, x, y):
-        bounce = False
-        BOUNCE = 10
+        # if it hit the wall, bounce it back
+        self.bounce(turtle.xcor(), turtle.ycor())
+        #print(self._XMAX, self._YMAX, turtle.xcor(), turtle.ycor())
+
+    def bounce(self, x, y):
+        b = False
         if x < self._XMIN:
-            x = self._XMIN + BOUNCE
-            bounce = True
+            x = self._XMIN + self.BOUNCE
+            b = True
         elif x > self._XMAX:
-            x = self._XMAX - BOUNCE
-            bounce = True
+            x = self._XMAX - self.BOUNCE
+            b = True
         if y < self._YMIN:
-            y = self._YMIN + BOUNCE
-            bounce = True
+            y = self._YMIN + self.BOUNCE
+            b = True
         elif y > self._YMAX:
-            y = self._YMAX - BOUNCE
-            bounce = True
+            y = self._YMAX - self.BOUNCE
+            b = True
 
-        if bounce:
+        if b:
             turtle.setpos(x, y)
 
 
